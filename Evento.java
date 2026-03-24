1c125bd0d90fa4d35c2778b6e1bec920ea0579a4
tvaz085-prog <tvaz085@gmail.com>
2026-03-24 17:20:59 -0300

    Update Evento.java

--- Evento.java
+++ Evento.java
@@ -1,1 +1,24 @@
+public class Evento {
+    String nome;
+    String endereco;
+    String categoria;
+    String horario;
+    String descricao;
 
+    public Evento(String nome, String endereco, String categoria, String horario, String descricao) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.categoria = categoria;
+        this.horario = horario;
+        this.descricao = descricao;
+    }
+
+    public void exibir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Horário: " + horario);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("----------------------------");
+    }
+}
