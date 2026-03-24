62e19e52947e6bbeeb035b5f4d11bd4b8675a41b
tvaz085-prog <tvaz085@gmail.com>
2026-03-24 17:20:44 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,1 +1,15 @@
+public class Usuario {
+    String nome;
+    String email;
+    int idade;
 
+    public Usuario(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public void exibir() {
+        System.out.println("Usuário: " + nome + " | Email: " + email + " | Idade: " + idade);
+    }
+}
